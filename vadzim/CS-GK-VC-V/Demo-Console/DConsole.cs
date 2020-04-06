fdcb4879871b25c1db88aab52bd14be2ac305edc
Vadzim Naumchyk <vadzimn@ppedv.de>
2020-04-06 13:49:24 +0200

    d console

---
+++ vadzim/CS-GK-VC-V/Demo-Console/DConsole.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Demo_Console
+{
+    class DConsole
+    {
+        static void Main(string[] args)
+        {
+
+            // ==================================================================
+            Console.WriteLine("\n ### Console.WriteLine() ### ");
+            // schreibt die Argumente zum Default-Output
+            // bei der Konsole-App ist die cmd-Konsole
+            // ein anderes Outpus könnte z.B. eine Messagebox sein
+            Console.WriteLine("Ausgabe");
+
+
+            // ==================================================================
+            Console.WriteLine("\n ### Console.WriteLine(string, arg0, arg1) ### ");
+            int Alter = 35;
+            string Stadt = "Berlin";
+            Console.WriteLine("Ich bin Vadzim, {0}. Ich wohne in {1}", Alter, Stadt);
+            Console.WriteLine("Ich wohne in {1}, heiße Vadzim und bin {0} Jahre alt.", Alter, Stadt);
+
+            Console.ReadKey();
+
+
+        }
+    }
+}
